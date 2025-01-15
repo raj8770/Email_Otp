ee8de19ceabe2184e66f5bdac2096e4de59a080a
sumit7200 <rajsumit7200@gmail.com>
2025-01-15 14:23:23 +0530

    change repo

--- src/main/java/com/example/Email_Verification/controller/LinkedListMiddle.java
+++ src/main/java/com/example/Email_Verification/controller/LinkedListMiddle.java
@@ -31,6 +31,5 @@ public class LinkedListMiddle {
         System.out.println("The middle node value is: " + middle.val);
     }
 
-
 }
 
--- src/main/java/com/example/Email_Verification/repository/UserRepository.java
+++ src/main/java/com/example/Email_Verification/repository/UserRepository.java
@@ -3,10 +3,17 @@ package com.example.Email_Verification.repository;
 import com.example.Email_Verification.entity.User;
 import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
     Optional<User> findById(Long  Id);
+
+
+    @Query(value = "SELECT MAX(Salary) FROM Employee WHERE Salary < (SELECT MAX(Salary) FROM Employee)", nativeQuery = true)
+    Double findSecondHighestSalary();
+
+
 }
